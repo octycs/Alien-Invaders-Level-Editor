934eec0fe2549273ee07eaaacd6953329fb443d2
Rahix <h.w.seiler@gmx.de>
2014-09-10 09:30:31 +0200

    Added tile-images

--- canvas_draw.js
+++ canvas_draw.js
@@ -22,14 +22,52 @@ function _cpdDrawMap(xOff, yOff)
 	//Now Start to Draw
 	var i;
 	var t;
-	for ( i = 0; i < _cpMapData.General.Size.X; i++) {
-		for ( t = 0; t < _cpMapData.General.Size.Y; t++) {
-			if (_cpMap[t][i].Tile < 15) {
+	for ( i = 0; i < _cpMapData.General.Size.X; i++) 
+	{
+		for ( t = 0; t < _cpMapData.General.Size.Y; t++) 
+		{
+			if (_cpMap[t][i].Tile < 15) 
+			{
 				_cpCCTX.drawImage(document.getElementById("img_tile"), i * tiledim, t * tiledim, tiledim, tiledim);
+			}
+			else if(_cpMap[t][i].Tile == 17) //Start
+			{
+				_cpdDrawWayArrow(tiledim,i,t);
+				_cpCCTX.drawImage(document.getElementById(_cpcImgStartTileID), i * tiledim, t * tiledim, tiledim, tiledim);
+			}
+			else if(_cpMap[t][i].Tile == 16)//End
+			{
+				_cpdDrawWayArrow(tiledim,i,t);
+				_cpCCTX.drawImage(document.getElementById(_cpcImgEndTileID), i * tiledim, t * tiledim, tiledim, tiledim);
+			}
+			else if(_cpMap[t][i].Tile == 15)//Way
+			{
+				_cpdDrawWayArrow(tiledim,i,t);
 			}
 		}
 	}
 }
+
+function _cpdDrawWayArrow(tiledim, i, t) 
+{
+	if (_cpMap[t][i].Dir == "N") 
+	{
+		_cpCCTX.drawImage(document.getElementById(_cpcImgUpTileID), i * tiledim, t * tiledim, tiledim, tiledim);
+	}
+	if (_cpMap[t][i].Dir == "E") 
+	{
+		_cpCCTX.drawImage(document.getElementById(_cpcImgRightTileID), i * tiledim, t * tiledim, tiledim, tiledim);
+	}
+	if (_cpMap[t][i].Dir == "S") 
+	{
+		_cpCCTX.drawImage(document.getElementById(_cpcImgDownTileID), i * tiledim, t * tiledim, tiledim, tiledim);
+	}
+	if (_cpMap[t][i].Dir == "W") 
+	{
+		_cpCCTX.drawImage(document.getElementById(_cpcImgLeftTileID), i * tiledim, t * tiledim, tiledim, tiledim);
+	}
+}
+
 
 /**
  * Erases the screen
